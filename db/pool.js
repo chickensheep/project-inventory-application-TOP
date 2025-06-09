de9248e43d35d01c7b05f444cc802f85ea3c381b
Sa Ziyang <ziyang.sa@gmail.com>
2025-06-09 16:39:55 +0800

    edit the pool

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,6 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-	host: "localhost",
-	user: process.env.DB_USER,
-	database: "project_inventory_application_top",
-	password: process.env.DB_PASSWORD,
-	port: 5432,
+	connectionString:
+		"postgresql://neondb_owner:npg_qet8l7QzYkop@ep-purple-sun-a89mz7o5-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
 });
